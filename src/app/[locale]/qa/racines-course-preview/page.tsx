0fda37d4fe9ebdecf0c70d921121a4119c478eca
nkenguep-maker <nkengue.p@gmail.com>
2026-09-19 16:34:57 -0700

    qa: put Bassa first in Racines E1 preview

--- src/app/[locale]/qa/racines-course-preview/page.tsx
+++ src/app/[locale]/qa/racines-course-preview/page.tsx
@@ -1,9 +1,9 @@
-import { lingalaE1, medumbaE1 } from "@/content/racines-e1-solo";
+import { bassaE1, lingalaE1, medumbaE1 } from "@/content/racines-e1-solo";
 import { RacinesPilotLanding } from "@/features/racines-course-experience/RacinesPreviewViews";
 
 export const dynamic = "force-dynamic";
 
 export default async function RacinesPilotPage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
-  return <RacinesPilotLanding locale={locale} courses={[medumbaE1, lingalaE1]} />;
+  return <RacinesPilotLanding locale={locale} courses={[bassaE1, medumbaE1, lingalaE1]} />;
 }
